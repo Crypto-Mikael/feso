733615a601aa96ed8a0bf2f90eddb6132f82828e
Miguel Campos <80548535+Crypto-Mikael@users.noreply.github.com>
2026-03-19 23:14:03 +0000

    chore: exemplo de decisao

---
+++ index6.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um numero inteiro: "))
+
+# Estrutura de decisão
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
