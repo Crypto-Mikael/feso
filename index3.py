2843aa821c3d86b27f53d3a82059c31e007d75a3
Miguel Campos <80548535+Crypto-Mikael@users.noreply.github.com>
2026-03-19 23:10:33 +0000

    chore: operacoes matematica exemplo

---
+++ index3.py
@@ -0,0 +1,9 @@
+# Operacoes matematicas basicas
+a = 10
+b = 3
+
+print("Soma:", a + b)
+print("Subtracao:", a - b)
+print("Multiplicacao:", a * b)
+print("Divisao:", a / b)
+print("Potencia:", a ** b)
